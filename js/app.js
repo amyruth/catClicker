f2be1729e63da41f4dc0120bd2997c55f8f6100b
amyruth <anrutherford@gmail.com>
2018-08-01 13:52:24 -0400

    added 2nd cat but all clicks go to one counter

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 const scoreArea = document.querySelector('.cat1-clicks');
-const cat1 = document.querySelector('.cat1');
+const cat = document.querySelectorAll('.cat');
 let counter = 0;
 
-cat1.addEventListener('click', function() {
-	counter ++;
-	scoreArea.innerHTML = `<p>${counter}</p>`;
+
+cat.forEach(function(item) {
+	item.addEventListener('click', function() {
+			counter ++;
+			scoreArea.innerHTML = `<p>${counter}</p>`;
+	});	
 });
