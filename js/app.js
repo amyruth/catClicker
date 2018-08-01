d84984eaf7454669a706a0395d50020852ecb811
amyruth <anrutherford@gmail.com>
2018-08-01 18:15:49 -0400

    inital commit on premium branch

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
-const scoreArea = document.querySelector('.cat1-clicks');
-const scoreArea2 = document.querySelector('.cat2-clicks');
-const cat1 = document.querySelector('.cat1');
-const cat2 = document.querySelector('.cat2');
-let counter1 = 0;
-let counter2 = 0;
+// const scoreArea = document.querySelector('.cat1-clicks');
+// const scoreArea2 = document.querySelector('.cat2-clicks');
+// const cat1 = document.querySelector('.cat1');
+// const cat2 = document.querySelector('.cat2');
+// let counter1 = 0;
+// let counter2 = 0;
 
-cat1.addEventListener('click', function () {
-	counter1++;
-	scoreArea.innerHTML = `<p>${counter1}</p>`;
-});
+// cat1.addEventListener('click', function () {
+// 	counter1++;
+// 	scoreArea.innerHTML = `<p>${counter1}</p>`;
+// });
 
-cat2.addEventListener('click', function () {
-	counter2++;
-	scoreArea2.innerHTML = `<p>${counter2}</p>`;
-});
+// cat2.addEventListener('click', function () {
+// 	counter2++;
+// 	scoreArea2.innerHTML = `<p>${counter2}</p>`;
+// });
