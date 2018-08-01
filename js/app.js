ec682b4641c5ddeac1db4f25fe6d747e616a25be
amyruth <anrutherford@gmail.com>
2018-08-01 15:21:35 -0400

    resized imgs, added 2nd cat and counter

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
 const scoreArea = document.querySelector('.cat1-clicks');
-const cat = document.querySelectorAll('.cat');
-let counter = 0;
+const scoreArea2 = document.querySelector('.cat2-clicks');
+const cat1 = document.querySelector('.cat1');
+const cat2 = document.querySelector('.cat2');
+let counter1 = 0;
+let counter2 = 0;
 
+cat1.addEventListener('click', function () {
+	counter1++;
+	scoreArea.innerHTML = `<p>${counter1}</p>`;
+});
 
-cat.forEach(function(item) {
-	item.addEventListener('click', function() {
-			counter ++;
-			scoreArea.innerHTML = `<p>${counter}</p>`;
-	});	
+cat2.addEventListener('click', function () {
+	counter2++;
+	scoreArea2.innerHTML = `<p>${counter2}</p>`;
 });
